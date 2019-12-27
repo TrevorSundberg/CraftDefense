5d80ee91888f1b835f4344ec201b308c3a3495ce
Trevor Sundberg <tsundberg@s2.systems>
2019-12-26 17:09:49 -0800

    Added a basic run script that creates the server

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-java -jar lib/craftbukkit-1.15.1-R0.1-SNAPSHOT.jar
+rm -rf server
+mkdir server
+cd server
+mkdir plugins
+cp ../target/craftdefense-1.0-SNAPSHOT.jar plugins
+echo eula=true > eula.txt
+java -DIReallyKnowWhatIAmDoingISwear -jar ../lib/craftbukkit-1.15.1-R0.1-SNAPSHOT.jar
