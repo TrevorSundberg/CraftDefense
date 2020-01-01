a5b42629911684a27760575c23d65c259503c8c1
Trevor Sundberg <tsundberg@s2.systems>
2019-12-31 17:50:53 -0800

    Make it easily reset the server each run

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ mvn package
 rm -rf server
 mkdir server
 cd server
-echo eula=true > eula.txt
 mkdir plugins
 cp ../target/craftdefense-1.0-SNAPSHOT.jar plugins
 cp ../lib/craftbukkit-1.15.1-R0.1-SNAPSHOT.jar .
--- src/run.bat
+++ src/run.bat
@@ -1,1 +1,5 @@
+rd /s /q data
+mkdir data
+cd data
+echo eula=true > eula.txt
 java -DIReallyKnowWhatIAmDoingISwear -jar craftbukkit-1.15.1-R0.1-SNAPSHOT.jar
