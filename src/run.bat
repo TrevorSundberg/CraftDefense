51cd557d0ebcc47667b102d653e3d22e166354e2
TrevorSundberg <Trevor.Sundberg@gmail.com>
2020-02-16 14:35:40 -0800

    Fixed Windows batch file

--- src/run.bat
+++ src/run.bat
@@ -1,5 +1,8 @@
 rd /s /q data
 mkdir data
 cd data
-echo eula=true > eula.txt
-java -DIReallyKnowWhatIAmDoingISwear -jar craftbukkit-1.15.1-R0.1-SNAPSHOT.jar
+echo eula=true> eula.txt
+mkdir plugins
+copy ..\plugins\* plugins
+java -Xmx1024M -DIReallyKnowWhatIAmDoingISwear -jar ..\craftbukkit-1.15.1-R0.1-SNAPSHOT.jar
+cd ..
